ec5d984e154d7645a346efc5a6bc2c2920978ce7
Adrian-ortiz0 <dxniel7328@gmail.com>
2024-11-09 21:31:02 -0500

    feat: :art: I've created my custom hook and added the loading message

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,22 +1,14 @@
-import { useEffect, useState } from "react";
-import { getGifs } from "../helpers/getGifs";
 import { GifGridItem } from "./GifGridItem";
+import { useFetchGifs } from "../hooks/useFetchGifs";
 
 export const GifGrid = function ({ category }) {
-  const [images, setImages] = useState([]);
-
-  const getImages = async function () {
-    const newImages = await getGifs(category);
-    setImages(newImages);
-  };
-
-  useEffect(() => {
-    getImages();
-  }, []);
+  const { images, isLoading } = useFetchGifs(category);
 
   return (
     <>
       <h3>{category}</h3>
+
+      {isLoading ? <h2>Cargando...</h2> : null}
 
       <div className="card-grid">
         {images.map(function (img) {
